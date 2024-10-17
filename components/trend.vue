e30d39cc8dc471767ead316c70bfda894cadf42c
Kévin Calmant <kevin.calmant@outlook.com>
2024-10-17 20:50:08 +0200

    feat: main app page + tend component
    Basic setup of the main page of the application and creating Trend dummy component

---
+++ components/trend.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps({
+	title: String,
+	amount: Number,
+	lastAmount: Number,
+	color: String as PropType<'green' | 'red'>,
+	loading: Boolean,
+})
+</script>
+
+<template>
+	<div>
+		<div class="font-bold" :class="[color]">{{ title }}</div>
+
+		<div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+			<USkeleton v-if="loading" class="h-8 w-full" />
+			<div v-else>{{ amount }}</div>
+		</div>
+
+		<div>
+			<USkeleton v-if="loading" class="h-6 w-full" />
+			<div v-else class="flex space-x-1 items-center text-sm">
+				<UIcon name="i-heroicons-arrow-trending-up" class="w-6 h-6" :class="[color]" />
+				<div class="text-gray-500 dark:text-gray-400">
+					30% vs last period
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.green {
+	@apply text-green-600 dark:text-green-400;
+}
+
+.red {
+	@apply text-red-600 dark:text-red-400;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { transactionView } from '~/constants';
+
+const selectedView = ref(transactionView[1])
+</script>
+
+<template>
+	<scection class="flex items-center justify-between mb-10">
+		<h1 class="text-4xl font-extrabold">
+			Summary
+		</h1>
+		<div>
+			<USelectMenu v-model="selectedView" :options="transactionView" />
+		</div>
+	</scection>
+
+	<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+		<Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+		<Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+		<Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+		<Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+	</section>
+</template>
